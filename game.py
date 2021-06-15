6d1153889d4e6be35114f219f7384558f1d0f89f
CameronBrenton <Cameron.Brenton@keyin.com>
2021-06-15 10:37:59 -0230

    fixed quit issue

--- game.py
+++ game.py
@@ -8,6 +8,15 @@ VERSION = "0.1"
 try:
     import sys
     import pygame
+    from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 except ImportError as err:
     print(f"couldn't load module. {err}")
     sys.exit(2)
@@ -20,13 +29,17 @@ running = True
 while running:
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == KEYDOWN:
+            print('Keydown')
+            if event.key == K_ESCAPE:
+                print('Quit')
+                running = False
+        elif event.type == QUIT:
             running = False
-
 
     screen.fill((225, 225, 225))
 
-    pygame.draw.circle(screen, (0, 0, 225), (250, 250), 75)
+    pygame.draw.circle(screen, (52, 235, 192), (250, 250), 75)
 
     pygame.display.flip()
 
