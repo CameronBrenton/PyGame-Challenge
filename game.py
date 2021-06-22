fe27df87e671761ee0af3f2d60b97089d42ee5e4
CameronBrenton <Cameron.Brenton@keyin.com>
2021-06-22 16:17:43 -0230

    Added credits to Jon Fincher @ Real python for the tutorial tht made this all possible

--- game.py
+++ game.py
@@ -6,6 +6,7 @@
 # flying through the air. The goal is to stay alive for as long as possible
 # by not colliding with any other birds or objects!
 #
+# Many thanks to Jon Fincher at https://realpython.com/pygame-a-primer/ for the tutorial who pretty much guides me through this whole thing! 
 # Many thanks to bluecarrot16 at https://opengameart.org/content/lpc-birds for providing the awesome bird spritesheets
 # Many thanks to hassekf https://opengameart.org/content/2d-clouds-pack for the awesome cloud spritesheets
 #
